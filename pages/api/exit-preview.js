97cf6a6022a6467bc3250be51fde4e964022e211
lisilinhart <ewl@storyblok.com>
2021-05-04 15:27:12 +0200

    fix: update cookie header
    to work in Chrome / Safari

--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -4,7 +4,7 @@ export default async function exit(_, res) {
  
      // set the cookies to None
      const cookies = res.getHeader('Set-Cookie')
-     res.setHeader('Set-Cookie', cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None')))
+     res.setHeader('Set-Cookie', cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None;Secure')))
  
     // Redirect the user back to the index page.
     res.redirect('/')
