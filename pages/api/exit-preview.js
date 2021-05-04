4e6d82e0488e70c8a4d95a14a7d6c96092307b3e
lisilinhart <ewl@storyblok.com>
2021-05-04 17:09:15 +0200

    Update exit-preview.js

--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -1,4 +1,5 @@
-export default async function exit(_, res) {
+export default async function exit(req, res) {
+    const { slug = '' } = req.query
     // Exit the current user from "Preview Mode". This function accepts no args.
     res.clearPreviewData()
  
@@ -7,5 +8,5 @@ export default async function exit(_, res) {
      res.setHeader('Set-Cookie', cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None;Secure')))
  
     // Redirect the user back to the index page.
-    res.redirect('/')
+    res.redirect(`/${slug}`)
   }
