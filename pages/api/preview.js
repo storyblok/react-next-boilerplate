a8a0a495bc85930649bc6ba69d01c808aa1692fd
lisilinhart <ewl@storyblok.com>
2021-05-04 17:09:37 +0200

    Update preview.js

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -2,7 +2,7 @@ export default async function preview(req, res) {
     const { slug = '' } = req.query
     // Check the secret and next parameters
     // This secret should only be known to this API route and the CMS
-    if (req.query.secret !== 'test') {
+    if (req.query.secret !== 'MY_SECRET_TOKEN') {
       return res.status(401).json({ message: 'Invalid token' })
     }
   
