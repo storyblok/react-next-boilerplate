76d4f93450d71af8180796349de7e923495430df
Lisi Linhart <ewl@storyblok.com>
2021-05-04 15:23:32 +0200

    fix: update cookie header

--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -5,7 +5,7 @@ export default async function exit(req, res) {
  
      // set the cookies to None
      const cookies = res.getHeader('Set-Cookie')
-     res.setHeader('Set-Cookie', cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None')))
+     res.setHeader('Set-Cookie', cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None;Secure')))
  
     // Redirect the user back to the index page.
     res.redirect(`/${slug}`)
--- pages/api/preview.js
+++ pages/api/preview.js
@@ -2,7 +2,7 @@ export default async function preview(req, res) {
     const { slug = '' } = req.query
     // Check the secret and next parameters
     // This secret should only be known to this API route and the CMS
-    if (req.query.secret !== 'test') {
+    if (req.query.secret !== 'MY_SECRET_TOKEN') {
       return res.status(401).json({ message: 'Invalid token' })
     }
   
@@ -11,7 +11,7 @@ export default async function preview(req, res) {
  
     // Set cookie to None, so it can be read in the Storyblok iframe
     const cookies = res.getHeader('Set-Cookie')
-    res.setHeader('Set-Cookie', cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None')))
+    res.setHeader('Set-Cookie', cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None;Secure')))
  
   
     // Redirect to the path from entry
