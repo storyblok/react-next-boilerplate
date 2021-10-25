352b3e5065143d63d607f8725fe8b166018702b9
Facundo Giuliani <facundo.giuliani@gmail.com>
2021-10-24 22:51:24 -0300

    Edit Tutorial

--- components/DynamicComponent.js
+++ components/DynamicComponent.js
@@ -1,29 +1,35 @@
-import { useRouter } from 'next/router'
-import SbEditable from 'storyblok-react'
-import Teaser from './Teaser'
-import Grid from './Grid'
-import Feature from './Feature'
-import Page from './Page'
- 
+import SbEditable from "storyblok-react";
+import Teaser from "./Teaser";
+import Grid from "./Grid";
+import Feature from "./Feature";
+import Page from "./Page";
+
 // resolve Storyblok components to Next.js components
 const Components = {
-  'teaser': Teaser,
-  'grid': Grid,
-  'feature': Feature,
-  'page': Page,
-}
- 
-const DynamicComponent = ({blok}) => {
-  const { isPreview } = useRouter()
+  teaser: Teaser,
+  grid: Grid,
+  feature: Feature,
+  page: Page,
+};
+
+const DynamicComponent = ({ blok }) => {
   // check if component is defined above
-  if (typeof Components[blok.component] !== 'undefined') {
-    const Component = Components[blok.component]
+  if (typeof Components[blok.component] !== "undefined") {
+    const Component = Components[blok.component];
     // wrap with SbEditable for visual editing
-    return isPreview ? (<SbEditable content={blok}><Component blok={blok} /></SbEditable>) : <Component blok={blok} />
+    return (
+      <SbEditable content={blok}>
+        <Component blok={blok} />
+      </SbEditable>
+    );
   }
-  
+
   // fallback if the component doesn't exist
-  return (<p>The component <strong>{blok.component}</strong> has not been created yet.</p>)
-}
- 
-export default DynamicComponent
+  return (
+    <p>
+      The component <strong>{blok.component}</strong> has not been created yet.
+    </p>
+  );
+};
+
+export default DynamicComponent;
